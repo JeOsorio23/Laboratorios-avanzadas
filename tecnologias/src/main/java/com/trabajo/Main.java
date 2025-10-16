fca4875a9640baf4eeabb367c61a6468615ed8a2
JeOsorio23 <jeosorio@unillanos.edu.co>
2025-10-15 20:01:46 -0500

    Delete tecnologias directory

--- tecnologias/src/main/java/com/trabajo/Main.java
+++
@@ -1,9 +0,0 @@
-package com.trabajo;
-
-import com.trabajo.Interfaz.InterfazMain;
-
-public class Main {
-    public static void main(String[] args) {
-        new InterfazMain();
-    }
-}
--- tecnologias/src/main/java/com/trabajo/Servicios.java
+++
@@ -1,7 +0,0 @@
-package com.trabajo;
-
-public interface Servicios {
-    String imprimirPosicion(int posicion);
-    int cantidadActual();
-    void imprimirListado();
-}
